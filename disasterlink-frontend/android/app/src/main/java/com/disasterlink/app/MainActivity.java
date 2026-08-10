5e82df04fdd0b9009d76168cac3ee08a676cbd91
Dave Hermoso <Davehermoso01@gmail.com>
2026-08-11 00:55:02 +0800

    fix(mobile): add SSL TrustManager bypass in MainActivity to ignore Hostinger intermediate cert errors

--- disasterlink-frontend/android/app/src/main/java/com/disasterlink/app/MainActivity.java
+++ disasterlink-frontend/android/app/src/main/java/com/disasterlink/app/MainActivity.java
@@ -1,5 +1,35 @@
 package com.disasterlink.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Bypassing SSL for Development/Testing
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[]{};
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+            };
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
